ac37a8d049c34ac2e3aaee6187a77677f848c7c3
krnlpawar <krnlpawar@gmail.com>
2025-01-04 22:08:17 +0530

    updated quote component

--- src/components/Quote.jsx
+++ src/components/Quote.jsx
@@ -1,21 +1,26 @@
 import React from 'react'
 
 export default function Quote() {
+	const quote = [
+		{
+			title: 'Al. Siam',
+			desc: 'Lorem ipsum dolor sit amet.',
+			span: `Himenaeos curabitur feugiat etiam in enim sed felis interdum
+			lobortis phasellus nec eros ut arcu sollicitudin pellentesque
+			curabitur porta justo vitae molestie semper ligula enim sed
+			felis interdum lobortis phasellus nec eros ut arcu sollicitudin
+			pellentesque curabitur porta justo vitae molestie semper ligula.`,
+		},
+	]
   return (
 	<section className="main-quote">
 	<div className="container">
 	  <div className="quote-box d-flex">
 		<img src="img/100x100.png" alt="" />
 		<div className="quote-wrap">
-		  <h1>Al. Siam</h1>
-		  <span>Lorem ipsum dolor sit amet.</span>
-		  <p>
-			Himenaeos curabitur feugiat etiam in enim sed felis interdum
-			lobortis phasellus nec eros ut arcu sollicitudin pellentesque
-			curabitur porta justo vitae molestie semper ligula enim sed
-			felis interdum lobortis phasellus nec eros ut arcu sollicitudin
-			pellentesque curabitur porta justo vitae molestie semper ligula.
-		  </p>
+		  <h1>{quote[0].title}</h1>
+		  <span>{quote[0].desc}</span>
+		  <p>{quote[0].span}</p>
 		</div>
 	  </div>
 	</div>
